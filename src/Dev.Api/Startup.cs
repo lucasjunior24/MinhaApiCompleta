9dcbc05e0432a9ce105dcf378d95651c05fd12c4
lucas souza <lukateku@gmail.com>
2021-09-26 13:01:45 -0400

    Implementando o Identity

--- src/Dev.Api/Startup.cs
+++ src/Dev.Api/Startup.cs
@@ -34,6 +34,8 @@ namespace Dev.Api
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddIdentityConfiguration(Configuration);
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -58,7 +60,7 @@ namespace Dev.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
